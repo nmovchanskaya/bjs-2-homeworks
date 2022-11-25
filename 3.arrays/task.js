b7bd022ada6c511597abeacaa01fcf7621ad21ce
nika.movchanskaya <nika.movchanskaya@gmail.com>
2022-11-25 17:54:22 +0100

    improvements for the array task: not neseccary variables, more simple construction of first task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  if (arr1.length === arr2.length) {
-    result = arr1.every((element, index) => (element === arr2[index]));
-  }
-  else {
-    result = false;
-  }
-  return result; // boolean
+  return arr1.length === arr2.length && 
+    arr1.every((element, index) => (element === arr2[index]));
+
 }
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
+  return arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
 
-  return resultArr; // array
 }
 
